9af61a9453604b8afa7ad6cb653550f60743ee4b
Brianna Bell <bbell9563@gmail.com>
2020-03-04 16:38:16 -0700

    have 7 of the exercises working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,101 @@
-import {originalArray} from '../app' 
+import {doubleArray,
+        evensOnlyArray,
+        arraySum,
+        allNumbersGreaterThanZero,
+        someNumbersAreOdd,
+        evensOnlyAndDoubleArray,
+        findItem
+} from '../app' 
 
-describe('original array test', () => {
-  test('testing original array', () => {
-    let arr = ['to', 'me']
-    expect(originalArray(arr)).toEqual(arr)
+var numberArray = [1,2,3,4]
+// NUMBER 1 TEST
+describe('NO.1 Testing doubleArray', () => {
+  test('test given array', ()=>{
+
+    expect(doubleArray(numberArray)).toEqual([2,4,6,8])
+  })
+  test('test negative  array', ()=>{
+    expect(doubleArray([-2,5,6,-1])).toEqual([-4,10,12,-2])
+  })
+  test('checks for strings', ()=>{
+    expect(doubleArray([3, -2, 'im not suppose to be here', 7])).toEqual("error: array has str")
+  })  
+})
+
+//  NUMBER 2 TEST
+describe('NO.2 Testing evensOnlyArray', () =>{
+  test('test original array with', () => {
+    var numberArray1 = [1,2,3,4,5,6,7,8]
+    expect(evensOnlyArray(numberArray1)).toEqual([2,4,6,8])
+  })
+  test('test with negative numbers', () => {
+    expect(evensOnlyArray([-1,-2,-3,-4,-5,-6])).toEqual([-2,-4,-6])
+  })
+  test('test with strings', () => {
+    expect(evensOnlyArray([-1,7,'hi', -4, 3, 8])).toEqual([-4,8])
   })
 })
+
+// NUMBER 3 TEST
+describe('NO.3 Testing arraySum', ()=> {
+  test('testing original array', ()=> {
+    expect(arraySum(numberArray)).toEqual(10)
+  })
+  test('test negative numbers',()=>{
+    expect(arraySum([-1,4,6,-10])).toEqual(-1)
+  })
+  test('testing strings',()=>{
+    expect(arraySum(['i', 't', 7,5])).toEqual("error: contains strings")
+  })
+})
+
+// NUMBER 4 TEST
+describe('NO.4 Testing allNumbersGreaterThanZero', ()=>{
+  test('test numberArray', ()=> {
+    expect(allNumbersGreaterThanZero(numberArray)).toEqual(true)
+  })
+  test('test numbers not greater than 0', ()=> {
+    expect(allNumbersGreaterThanZero([-1,-2,2,6,7])).toEqual(false)
+  })
+  test('testing strings',()=>{
+    expect(allNumbersGreaterThanZero(['i', 't', 7,5])).toEqual("error: contains strings")
+  })
+})
+
+// NUMBER 5 TEST
+describe('NO.5 Testing someNumbersAreOdd', ()=>{
+  test('test numberArray', ()=> {
+    expect(someNumbersAreOdd(numberArray)).toEqual(true)
+  })
+  test('test no numbers are odd', ()=> {
+    expect(someNumbersAreOdd([2,4,6,8])).toEqual(false)
+  })
+  test('testing strings',()=>{
+    expect(someNumbersAreOdd(['i', 't', 7,5])).toEqual("error: contains strings")
+  })
+})
+
+// NUMBER 6 TEST
+describe('NO.6 Testing evensOnlyAndDoubleArray', ()=>{
+  test('test numberArray', ()=> {
+    expect(evensOnlyAndDoubleArray(numberArray)).toEqual([4,8])
+  })
+  test('test some numbers negative', ()=> {
+    expect(evensOnlyAndDoubleArray([-2,4,-6,8])).toEqual([-4,8,-12,16])
+  })
+  test('testing strings',()=>{
+    expect(evensOnlyAndDoubleArray(['i', 't', 7,5])).toEqual("error: contains strings")
+  })
+})
+
+// NUMBER 7 TEST
+describe('NO.7 Test findItem', () => {
+  test('test finding 1 in original array', ()=> {
+    expect(findItem(numberArray, 1)).toEqual(1)
+  })
+  test('test finding 1 in original array', ()=> {
+    expect(findItem(numberArray, 10)).toEqual("Item Not Found")
+  })
+})
+
+// NUMBER 8 TEST
--- app.js
+++ app.js
@@ -1,4 +1,116 @@
-
-export const originalArray = (arr) => {
-  return arr
+// NUMBER 1
+// return array with double values in array
+export const doubleArray= (arr) =>{
+  let results =  arr.map(double)
+  if(results.includes("error")){
+    return "error: array has str"
+  }
+  return results
 }
+
+const double = (element) => {
+  if(typeof element === "string"){
+    return "error"
+  }
+  return element * 2
+}
+
+
+// NUMBER 2
+// return array with all even numbers
+
+export const evensOnlyArray = (arr) => {
+  return arr.filter((element)=>{
+    if(element % 2 == 0 ){return element}
+  })
+}
+
+
+// NUMBER 3
+// return array with sum of numbers
+
+export const arraySum = (arr) => {
+  let results = arr.reduce((total, num)=>{
+   if(typeof num === "string"){
+    return ""
+   }
+   return total + num
+  })
+  if(typeof results === "string"){return "error: contains strings"}
+  return results
+}
+
+// NUMBER 4
+// return true if every item greater than 0
+export const allNumbersGreaterThanZero = (arr) => {
+  let stringCheck = arr.map(checkForString)
+  if(stringCheck.includes("error")){
+    return "error: contains strings"
+  }
+  return stringCheck.every((num)=>{
+    return num > 0
+  })
+}
+const checkForString = (element) => {
+  if(typeof element === "string"){
+    return "error"
+  }
+  return element
+}
+
+
+// NUMBER 5
+// return true if some one or more items odd*
+export const someNumbersAreOdd = (arr) => {
+  let stringCheck = arr.map(checkForString)
+  if(stringCheck.includes("error")){
+    return "error: contains strings"
+  }
+  return stringCheck.some((element)=>{
+    return element % 2 == 1
+  })
+}
+
+// NUMBER 6
+// return array double and even
+export const evensOnlyAndDoubleArray = (arr) => {
+  let stringCheck = arr.map(checkForString)
+  if(stringCheck.includes("error")){
+    return "error: contains strings"
+  }
+  let evenCheck = stringCheck.filter((element)=>{
+    if(element % 2 == 0){return element}
+  })
+
+  return evenCheck.map((element)=>{
+    return element * 2
+  })
+}
+
+// NUMBER 7
+// find an item method return 'not found' if not found
+export const findItem = (arr, item) => {
+  let searchItem =  arr.find((element)=> {
+    console.log(element)
+    return element === item
+  })
+  if(searchItem == undefined){
+    return "Item Not Found"
+  }
+  return searchItem
+}
+
+// NUMBER 8 
+// export const sortArray = (arr) => {
+//   arr.sort()
+// }
+// sortArray(numberArray) //=> [4,3,2,1] *desc*
+
+
+
+
+
+
+
+
+
